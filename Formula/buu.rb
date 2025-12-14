6fe3456af6e6ce27840e77637fc1a177a23f141a
Muhammed Sadik <50425929+mhmdxsadk@users.noreply.github.com>
2025-12-14 01:46:21 +0100

    Updated to version 1.0.2

--- Formula/buu.rb
+++ Formula/buu.rb
@@ -1,9 +1,14 @@
 class Buu < Formula
   desc "Lightweight CLI tool for maintaining Homebrew, written in Rust"
   homepage "https://github.com/mhmdxsadk/buu"
-  url "https://github.com/mhmdxsadk/buu/archive/refs/tags/v1.0.1.tar.gz"
-  sha256 "1e0d26711325cb50e82de9a9524e729b3d09e3ed32852c8483940ac713ea2439"
+  url "https://github.com/mhmdxsadk/buu/archive/refs/tags/v1.0.2.tar.gz"
+  sha256 "99ae26e7749658551dde4c2839f90e0e110a764049ffd3f4fab9ecacd79ae11f"
   license "MIT"
+
+  bottle do
+    root_url "https://github.com/mhmdxsadk/tools/releases/download/buu-1.0.1"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "e27b841f05f9edb001da17f531876ceb3e35d4125c38afd11856069455d3727e"
+  end
 
   depends_on "rust" => :build
 
