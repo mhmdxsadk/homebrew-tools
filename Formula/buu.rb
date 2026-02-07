a1645843c33c4274027d825e3a446bf7033bc711
Muhammed Sadik <50425929+mhmdxsadk@users.noreply.github.com>
2026-02-07 18:12:44 +0100

    buu 1.0.3

--- Formula/buu.rb
+++ Formula/buu.rb
@@ -7,8 +7,8 @@ class Buu < Formula
 
   bottle do
     root_url "https://github.com/mhmdxsadk/homebrew-tools/releases/download/buu-1.0.3"
-    rebuild 2
-    sha256 cellar: :any_skip_relocation, arm64_tahoe: "68f47f1c1f9a5a2db0372e7bd06858da1661f55a205e390042133a0ee70bf442"
+    rebuild 3
+    sha256 cellar: :any_skip_relocation, arm64_tahoe: "a86c02c064df1a15b132aafe458c43a835957e0cf5cc46eac5a27fb423f48cbe"
   end
 
   depends_on "rust" => :build
